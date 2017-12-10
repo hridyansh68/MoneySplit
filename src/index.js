d449dbde0bc6fff357f22862b1c8a1b9199cdf2e
Rishabh Tripathi <ristripathi97@gmail.com>
2017-12-10 23:59:32 +0530

    Changes in index.js

--- src/index.js
+++ src/index.js
@@ -4,5 +4,14 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+var list={
+	listItem : [],
+	addItem : function (newData){
+		this.listItem.push(newData);
+	}
+	
+};
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
