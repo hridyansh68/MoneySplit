4ac58e39ca72ffd992f428fdfa6942a996b5f548
Rishabh Tripathi <ristripathi97@gmail.com>
2017-12-16 14:12:09 +0530

    Added Dropdown

--- src/dropdown_bar.js
+++ src/dropdown_bar.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {ReactSelectize, SimpleSelect, MultiSelect} from 'react-selectize';
+import Input from 'muicss/lib/react/input';
+import Button from 'muicss/lib/react/button';
 
 class DropdownBar extends React.Component{
    render(){
-      return (<div>Second page</div>);}
+      return (<div><SimpleSelect
+            placeholder = "Select Name"
+            options = {this.props.myList.listItem.map(
+              (name,i) => ({label: name, value: i})
+            )}
+            onValuesChange = {value => alert(value)}
+        />
+        <Input label="Enter Expense" floatingLabel={true} type="number"/>
+        <MultiSelect
+            placeholder = "Select Name"
+            options = {this.props.myList.listItem.map(
+              (name,i) => ({label: name, value: i})
+            )}
+            onValuesChange = {value => alert(value)}
+        /></div>);}
       
       }
       
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'react-selectize/themes/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
