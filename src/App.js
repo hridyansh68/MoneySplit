53e08a25108392e89e965e244505156eb10b4494
Rishabh Tripathi <ristripathi97@gmail.com>
2018-01-09 00:11:40 +0530

    optimised for mobile

--- src/App.js
+++ src/App.js
@@ -155,7 +155,6 @@ class App extends Component {
     {
       return (
         <div>
-          <a href="https://github.com/hridyanshsahu/MoneySplit"><img style={imgStyle} src="https://camo.githubusercontent.com/567c3a48d796e2fc06ea80409cc9dd82bf714434/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_darkblue_121621.png"/></a>
         <Container>
         
         <div className="App2">
@@ -171,7 +170,6 @@ class App extends Component {
     {  
         return (
           <div>
-            <a href="https://github.com/hridyanshsahu/MoneySplit"><img style={imgStyle} src="https://camo.githubusercontent.com/567c3a48d796e2fc06ea80409cc9dd82bf714434/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_darkblue_121621.png"/></a>
             <Container>           
         <div className="App3">
           <FinalPage payingList={this.state.payingList}/>
